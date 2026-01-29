6a6127a7366b1e7b526dad11f3332e67d56cce5f
Justin <Justin.Weng.1@stonybrook.edu>
2026-01-28 19:40:06 -0500

    Moved login credentials to .env file

--- updateSQL.py
+++ updateSQL.py
@@ -2,12 +2,17 @@ print ("Started")
 import pandas as pd
 import urllib
 import sqlalchemy as sqla
+import dotenv
+import os
 
 '''
 Updating sql database
-1. Download the libraries (py -m pip install sqlalchemy pyodbc)
+1. Download the libraries (py -m pip install sqlalchemy pyodbc dotenv)
 2. Download ODBC driver 18 from microsoft
 '''
+
+#Gets login data to access database
+dotenv.load_dotenv("database.env")
 
 test_df = pd.DataFrame({
     "Names": ["Person1", "Person2", "Person3"],
@@ -17,10 +22,10 @@ test_df = pd.DataFrame({
 #Connecting to the database
 conn_str = (
     "DRIVER=ODBC Driver 18 for SQL Server;"
-    "SERVER=project-quack-server.database.windows.net;"
+    f"SERVER={os.getenv("Server")};"
     "DATABASE=Project Quack;"
-    "UID=ProjectQuackAdmin;"
-    "PWD=ProjectQuack123"
+    f"UID={os.getenv("UserId")};"
+    f"PWD={os.getenv("Password")}"
 )
 conn_url = f"mssql+pyodbc:///?odbc_connect={urllib.parse.quote_plus(conn_str)}"
 engine = sqla.create_engine(conn_url)
